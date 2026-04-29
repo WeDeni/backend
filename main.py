6269779c019143db715ce12d58db6e50f266ebbf
WeDen <wedeni@ya.ru>
2026-04-29 20:59:32 +0300

    Выгрузка задания

--- main.py
+++ main.py
@@ -2,8 +2,20 @@ from uuid import uuid4
 
 from fastapi import FastAPI, HTTPException, status
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+    ],
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_credentials=True,
+)
 
 
 class Task(BaseModel):
@@ -12,11 +24,13 @@ class Task(BaseModel):
     title: str
     completed: bool = False
 
-
 class TaskCreate(BaseModel):
     title: str
 
+class Book(BaseModel):
+    name: str
 
+book: Book | None = None
 tasks: list[Task] = []
 
 
@@ -26,9 +40,25 @@ def get_tasks():
     return tasks
 
 
+@app.get('/book', response_model=str)
+def get_book():
+    '''Получить информацию о книге'''
+    global book
+    name_book = book.name
+    return 'Любимая книга - ' + name_book
+
+
 @app.post('/tasks', response_model=Task, status_code=status.HTTP_201_CREATED)
 def create_task(payload: TaskCreate):
     '''Создать новую задачу'''
     task = Task(id=str(uuid4()), title=payload.title, completed=False)
     tasks.append(task)
     return task
+
+
+@app.post('/book', response_model=Book, status_code=status.HTTP_201_CREATED)
+def create_book(payload: Book):
+    '''Создать имя книги'''
+    global book
+    book = Book(name = payload.name)
+    return book
