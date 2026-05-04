0b40bc5e48f41cf2cffea81ce47b0299c79e1e3f
WeDen <wedeni@ya.ru>
2026-05-04 21:41:43 +0300

    Добавил пару методов

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:3000",
+        'http://localhost:3000',
     ],
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=['*'],
+    allow_headers=['*'],
     allow_credentials=True,
 )
 
@@ -29,6 +29,12 @@ class TaskCreate(BaseModel):
 
 class Book(BaseModel):
     name: str
+
+
+class TaskUpdateSchema(BaseModel):
+    title: str | None = None
+    completed: bool | None = None
+
 
 book: Book | None = None
 tasks: list[Task] = []
@@ -51,9 +57,12 @@ def get_book():
 @app.post('/tasks', response_model=Task, status_code=status.HTTP_201_CREATED)
 def create_task(payload: TaskCreate):
     '''Создать новую задачу'''
-    task = Task(id=str(uuid4()), title=payload.title, completed=False)
-    tasks.append(task)
-    return task
+    for task in tasks:
+        if task.title == payload.title:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Задача с таким названием уже существует')
+    new_task = Task(id=str(uuid4()), title=payload.title, completed=False)
+    tasks.append(new_task)
+    return new_task
 
 
 @app.post('/book', response_model=Book, status_code=status.HTTP_201_CREATED)
@@ -62,3 +71,20 @@ def create_book(payload: Book):
     global book
     book = Book(name = payload.name)
     return book
+
+@app.patch('/tasks/{task_id}')
+def update_task(task_id: str, payload: TaskUpdateSchema):
+    for task in tasks:
+        if task.id == task_id:
+            if payload.title:
+                task.title = payload.title
+            if payload.completed:
+                task.completed = payload.completed
+            return task
+
+@app.delete('/tasks/{task_id}')
+def delete_task(task_id: str):
+    for task in tasks:
+        if task.id == task_id:
+            tasks.remove(task)
+            return
